c375541fb23dd002dc1163f962f706ec57b92145
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-02-03 22:03:42 +0300

    Update 3-to_str.py

---
+++ 3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module for task 3"""
+
+
+def to_str(n: float) -> str:
+    """
+    Type annotated function to return a string representation
+    of a float"""
+    return str(n)
