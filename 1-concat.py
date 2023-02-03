52a5c23a5e6a8fa1acbcb2d99584491b06e8d010
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-02-03 21:55:47 +0300

    Update 1-concat.py

---
+++ 1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module for task 1"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type annotated function to return a concatenation
+    of two strings"""
+    return str1 + str2
